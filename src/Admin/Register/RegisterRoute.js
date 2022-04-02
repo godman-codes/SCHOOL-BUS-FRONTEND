29d97f555e0dfe976443bca02b0a08f09e0044a1
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-04-02 18:14:56 +0100

    implemented the register route functionalities for the register route page

--- src/Admin/Register/RegisterRoute.js
+++ src/Admin/Register/RegisterRoute.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import API from "../../API";
+//components
 import AdminNavbar from "../components/AdminNavbar";
 import RegisterRouteForm from "./components/RegisterRouteForm";
 
+import NavErrors from "../../components/NavErrors";
+//context
+import { Context } from "../../Context";
+
 const RegisterRoute = () => {
+   const [error, setError] = useState(false);
+   const [errorMessage, setErrorMessage] = useState("");
+   const [user] = useContext(Context);
+   const handleRegister = async (body) => {
+      try {
+         console.log(user.admin.access);
+         const registerRoute = await API.registerRoute(body, user.admin.access);
+         console.table(registerRoute);
+         if (registerRoute.message) {
+            console.log(registerRoute);
+            setError(false);
+         } else {
+            setError(true);
+            setErrorMessage(registerRoute.error);
+         }
+      } catch (error) {
+         setError(true);
+         setErrorMessage("server error");
+      }
+   };
    return (
       <>
          <AdminNavbar title="Godman Transports" />
-         <RegisterRouteForm />
+         {error && (
+            <NavErrors errorMessage={errorMessage} setError={setError} />
+         )}
+         <RegisterRouteForm handler={handleRegister} />
       </>
    );
 };
--- src/Admin/Register/components/RegisterRouteForm/index.js
+++ src/Admin/Register/components/RegisterRouteForm/index.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+//styled components
 import {
    Wrapper,
    Content,
 } from "../../../components/RegisterForm/RegisterForm.styles";
 import Button from "../../../../components/Button";
 
-const RegisterRouteForm = () => {
+const RegisterRouteForm = ({ handler }) => {
+   const [routes, setRoutes] = useState("");
+   const [time, setTime] = useState("");
+
+   const handleRegister = async (e) => {
+      if (routes === "" || time === "") {
+         alert("please fill all fields");
+      } else {
+         const data = {
+            routes_path: routes,
+            expected_time: time,
+         };
+         e.preventDefault();
+         try {
+            handler(data);
+         } catch (error) {
+            console.log(error);
+         }
+      }
+   };
+
+   const handleInput = (e) => {
+      const name = e.currentTarget.name;
+      const value = e.currentTarget.value;
+
+      if (name === "routes") setRoutes(value);
+      if (name === "time") setTime(value);
+   };
    return (
       <Wrapper>
          <div>
@@ -13,14 +41,24 @@ const RegisterRouteForm = () => {
             <Content>
                <div>
                   <label>Routes</label>
-                  <input type="text" />
+                  <input
+                     type="text"
+                     name="routes"
+                     value={routes}
+                     onChange={handleInput}
+                  />
                </div>
                <div>
                   <label>Expected Time</label>
-                  <input type="text" />
+                  <input
+                     type="text"
+                     name="time"
+                     value={time}
+                     onChange={handleInput}
+                  />
                </div>
             </Content>
-            <Button text="Register Route" />
+            <Button text="Register Route" callback={handleRegister} />
          </div>
       </Wrapper>
    );
